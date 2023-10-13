f12431bc448b8f38a532b81b5230af851230fd23
dyingsleeper <nik.savkin2004@mail.ru>
2023-10-13 03:22:12 +0300

    Подключил swiper к carousel-swiper; Добавил carousel.js; todo: заменить img и описание великов на 2nd slide

---
+++ LR1/js/carousel.js
@@ -0,0 +1,38 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs'
+class MainSlider{
+    constructor(item) {
+        this.block = item
+        this.swiperContainer = this.block.querySelector(".carousel-swiper__list")
+
+        this.swiperThumb = this.block.querySelector(".carousel-swiper__nav")
+
+        this.init()
+    }
+    init = () => {
+
+        this.thumb = new Swiper(this.swiperThumb, {
+            spaceBetween: 10,
+            slidesPerView: 'auto',
+            loop: true,
+        });
+
+        this.swiper = new Swiper(this.swiperContainer, {
+            // Optional parameters
+            loop: true,
+
+            // Navigation arrows
+
+            thumbs: {
+                swiper: this.thumb
+            }
+        });
+        // this.pagination.controller.control = this.swiper;
+        // this.swiper.controller.control = this.pagination;
+    }
+    static init(){
+        document.querySelectorAll(".carousel-swiper").forEach((item) => new MainSlider(item))
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", MainSlider.init)
