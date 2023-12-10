61c439bfb1850e114143bec17f4ab49df05ca3c4
dyingsleeper <nik.savkin2004@mail.ru>
2023-12-10 18:07:41 +0300

    Сделал вставку юзера в БД

--- LR3/logic.php
+++ LR3/logic.php
@@ -1,9 +1,58 @@
 <?php
+require_once 'db.php';
 
-$_SALT = "sanya_shedrin_gay";
+
+$SALT = "sanya_shedrin_gay";
+
+function getValuesFromPost(): array {
+    $defaultValues = [
+        'FIO' => '',
+        'birth' => '',
+        'email' => '',
+        'address' => '',
+        'vk' => '',
+        'interesting' => '',
+        'gender' => '',
+        'blood' => '',
+        'blood_rh' => '',
+        'password' => ''
+    ];
+    foreach ($_POST as $key => $value) {
+        $defaultValues[$key] = htmlspecialchars($value);
+    }
+    return $defaultValues;
+}
+function addUserInDB($user_data) {
+    global $db;
+    global $SALT;
+    $hashed_password = md5($user_data['password'].$SALT);
+
+    $register_data = $user_data;
+    $register_data['password'] = $hashed_password;
+    unset($register_data['password1']);
+    unset($register_data['button']);
+
+    var_dump($register_data);
+    $sql =
+        'INSERT INTO users
+        (mail,pass,name,birth_date,address,vk_link,interesting,gender,blood,blood_rh)
+        VALUES (:email, :password, :FIO, :birth, :address, :vk, :interesting, :gender, :blood, :blood_rh)';
+    $stmt = $db->prepare($sql);
+    $stmt->execute($register_data);
+
+    header('login.php');
+
+    exit();
+}
 function vardump($var) {
     echo '<pre>';
     var_dump($var);
     echo '</pre>';
 }
-vardump($_POST);
+
+
+if (isset($_POST['button'])) {
+    addUserInDB(getValuesFromPost());
+}
+//addUserInDB(getValuesFromPost());
+//vardump($_POST);
--- LR3/singup.php
+++ LR3/singup.php
@@ -1,7 +1,13 @@
 <?php
 error_reporting(E_ERROR | E_PARSE);
+require_once 'logic.php';
 require_once 'header.php';
 require_once 'error.php';
+$valuesFromPost = getValuesFromPost();
+if (isset($_POST['button'])) {
+    addUserInDB($valuesFromPost);
+}
+
 ?>
 
 <div style="text-align:center;margin-top:50px;" class="container">
@@ -9,66 +15,55 @@ require_once 'error.php';
         <div class="row g-4">
             <div class="col-6 ">
                 <label for="exampleFormControlInput2" class="form-label">Ф.И.О.</label>
-                <input value="<?php if (isset($_POST['FIO'])) {
-                    echo htmlspecialchars($_POST['FIO']);
-                } ?>" type="text" class="form-control" id="exampleFormControlInput2" name="FIO"
+                <input value="<?= (isset($_POST['FIO'])) ? htmlspecialchars($_POST['FIO']) : "" ?>"
+                       type="text" class="form-control" id="exampleFormControlInput2" name="FIO"
                        placeholder="Иванов Иван Иванович">
             </div>
             <div class="col-6 ">
                 <label for="exampleFormControlInput3" class="form-label">Дата рождения</label>
                 <input type="date" class="form-control" id="exampleFormControlInput3" name="birth"
-                       value="<?php if (isset($_POST['birth'])) {
-                           echo htmlspecialchars($_POST['birth']);
-                       } ?>">
+                       value="<?= (isset($_POST['birth'])) ? htmlspecialchars($_POST['birth']) : "" ?>">
             </div>
             <div class="col-6 ">
                 <label for="exampleFormControlInput1" class="form-label">Email</label>
-                <input value="<?php if (isset($_POST['email'])) {
-                    echo htmlspecialchars($_POST['email']);
-                } ?>" type="email" class="form-control" id="exampleFormControlInput1" name="email"
+                <input value="<?= (isset($_POST['email'])) ? htmlspecialchars($_POST['email']) : "" ?>"
+                       type="email" class="form-control" id="exampleFormControlInput1" name="email"
                        placeholder="name@example.com">
             </div>
             <div class="col-6 ">
                 <label for="exampleFormControlInput4" class="form-label">Адрес</label>
-                <input type="text" name="address" value="<?php if (isset($_POST['address'])) {
-                    echo htmlspecialchars($_POST['address']);
-                } ?>" class="form-control" id="exampleFormControlInput4"
+                <input value="<?= (isset($_POST['address'])) ? htmlspecialchars($_POST['address']) : "" ?>"
+                       type="text" class="form-control" id="exampleFormControlInput4" name="address"
                        placeholder="пр. имени В.И. Ленина, 56А, Волгоград, Волгоградская обл">
             </div>
             <div class="col-6 ">
                 <label for="exampleFormControlInput6" class="form-label">Ссылка на профиль
                     Вконтакте</label>
                 <input type="url" class="form-control" id="exampleFormControlInput6" name="vk"
-                       value="<?php if (isset($_POST['vk'])) {
-                           echo htmlspecialchars($_POST['vk']);
-                       } ?>" placeholder="https://vk.com/id">
+                       value="<?= (isset($_POST['vk'])) ? htmlspecialchars($_POST['vk']) : "" ?>"
+                       placeholder="https://vk.com/id">
             </div>
             <div class="col-6 ">
                 <label for="exampleFormControlInput5" class="form-label">Интересы</label>
                 <input  type="text" class="form-control" name="interesting"
-                        value="<?php if (isset($_POST['interesting'])) {
-                            echo htmlspecialchars($_POST['interesting']);
-                        } ?>" id="exampleFormControlInput5">
+                        value="<?= (isset($_POST['interesting'])) ? htmlspecialchars($_POST['interesting']) : "" ?>"
+                        id="exampleFormControlInput5">
             </div>
             <div class="col-6 ">
                 <select  name="gender" class="form-select"
                                        aria-label="Default select example">
                     <option selected disabled="disabled">Пол</option>
-                    <option value="Ж" <?php if ($_POST['gender'] === "Ж") {
-                        echo " selected";
-                    } ?> >Ж
+                    <option value="Ж" <?= ($_POST['gender'] === "Ж") ? " selected" : "" ?>>
+                        Ж
                     </option>
-                    <option value="М"<?php if ($_POST['gender'] === "М") {
-                        echo " selected";
-                    } ?> >М
+                    <option value="М" <?= ($_POST['gender'] === "М") ? " selected" : "" ?>>
+                        М
                     </option>
-                    <option value="Не скажу"<?php if ($_POST['gender'] === "Не скажу") {
-                        echo " selected";
-                    } ?>>Не скажу
+                    <option value="Не скажу" <?= ($_POST['gender'] === "Не скажу") ? " selected" : "" ?>>
+                        Не скажу
                     </option>
-                    <option value="Еще не определился"<?php if ($_POST['gender'] === "Еще не определился") {
-                        echo " selected";
-                    } ?>>Еще не определился
+                    <option value="Еще не определился" <?= ($_POST['gender'] === "Еще не определился") ? " selected" : "" ?>>
+                        Еще не определился
                     </option>
                 </select>
             </div>
@@ -76,21 +71,17 @@ require_once 'error.php';
                 <select  name="blood" class="form-select"
                                        aria-label="Default select example">
                     <option selected disabled="disabled">Группа Крови</option>
-                    <option value="0 (I)"<?php if ($_POST['blood'] === "0 (I)") {
-                        echo " selected";
-                    } ?>>0 (I)
+                    <option value="0 (I)"<?= ($_POST['blood'] === "0 (I)") ? " selected" : "" ?>>
+                        0 (I)
                     </option>
-                    <option value="A (II)"<?php if ($_POST['blood'] === "A (II)") {
-                        echo " selected";
-                    } ?>>A (II)
+                    <option value="A (II)"<?= ($_POST['blood'] === "A (II)") ? " selected" : "" ?>>
+                        A (II)
                     </option>
-                    <option value="B (III)"<?php if ($_POST['blood'] === "B (III)") {
-                        echo " selected";
-                    } ?>>B (III)
+                    <option value="B (III)"<?= ($_POST['blood'] === "B (III)") ? " selected" : "" ?>>
+                        B (III)
                     </option>
-                    <option value="AB (IV)"<?php if ($_POST['blood'] === "AB (IV)") {
-                        echo " selected";
-                    } ?>>AB (IV)
+                    <option value="AB (IV)"<?= ($_POST['blood'] === "AB (IV)") ? " selected" : "" ?>>
+                        AB (IV)
                     </option>
                 </select>
             </div>
@@ -98,13 +89,11 @@ require_once 'error.php';
                 <select name="blood_rh" class="form-select"
                                        aria-label="Default select example">
                     <option selected disabled="disabled">Резус-фактор</option>
-                    <option value="Положительный (+)"<?php if ($_POST['blood_rh'] === "Положительный (+)") {
-                        echo " selected";
-                    } ?>>Положительный (+)
+                    <option value="Положительный (+)"<?= ($_POST['blood_rh'] === "Положительный (+)") ? " selected" : "" ?>>
+                        Положительный (+)
                     </option>
-                    <option value="Отрицательный (-)"<?php if ($_POST['blood_rh'] === "Отрицательный (-)") {
-                        echo " selected";
-                    } ?>>Отрицательный (-)
+                    <option value="Отрицательный (-)"<?= ($_POST['blood_rh'] === "Отрицательный (-)") ? " selected" : "" ?>>
+                        Отрицательный (-)
                     </option>
                 </select>
             </div>
