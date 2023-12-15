51193b340de3e24f75c1127a9df78a6ff39307d5
dyingsleeper <nik.savkin2004@mail.ru>
2023-12-15 23:55:46 +0300

    LR5

--- LR5/import.php
+++ LR5/import.php
@@ -1,89 +1,57 @@
 <?php
 require_once 'db.php';
 global $db;
-// Получаем ссылку на файл из формы
 $url = $_POST['url'];
 
-// Проверяем валидность ссылки
 if (filter_var($url, FILTER_VALIDATE_URL)) {
-  // Инициализируем cURL-сессию
   $ch = curl_init($url);
-  // Устанавливаем опцию для возврата содержимого в виде строки
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_HEADER, false);
 
-  // Выполняем cURL-запрос
   $json = curl_exec($ch);
-    echo $json;
-  // Закрываем cURL-сессию
+
   curl_close($ch);
 
-  // Декодируем JSON в массив
   $data = json_decode($json, true);
 
   echo $data;
 
-  // Подключаемся к базе данных с помощью PDO
-  // В этом примере мы используем MySQL, но вы можете использовать любой другой драйвер, поддерживаемый PDO
-  // Вы также должны указать свои параметры подключения вместо localhost, username, password и database
-  // Устанавливаем режим обработки ошибок в PDO
-  // В этом примере мы используем исключения, но вы можете использовать любой другой режим, поддерживаемый PDO
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  // Получаем имя файла без расширения
   $filename = pathinfo($url, PATHINFO_FILENAME);
 
-  // Создаем новую таблицу с постфиксом _imported
   $tablename = $filename . "_imported";
 
-  // Определяем структуру таблицы в соответствии с данными из JSON-файла
-  // В этом примере мы предполагаем, что JSON-файл содержит массив объектов с одинаковыми полями
-  // Вы можете изменить этот код в зависимости от формата вашего JSON-файла
-  $fields = array_keys($data[0]); // Получаем имена полей из первого объекта
-  $columns = ""; // Строка для хранения списка столбцов
-  $values = ""; // Строка для хранения списка значений
+  $fields = array_keys($data[0]);
+  $columns = "";
+  $values = "";
   foreach ($fields as $field) {
-    // Добавляем имя и тип столбца в список
-    // В этом примере мы предполагаем, что все поля имеют тип VARCHAR(255)
-    // Вы можете изменить этот код в зависимости от типов данных в вашем JSON-файле
-    $columns .= "$field VARCHAR(255), ";
-
-    // Добавляем плейсхолдер для значения в список
+    $columns .= "$field VARCHAR(32), ";
     $values .= ":$field, ";
   }
-  // Удаляем последнюю запятую из списков
   $columns = rtrim($columns, ", ");
   $values = rtrim($values, ", ");
 
-  // Формируем SQL-запрос для создания таблицы
   $sql = "CREATE TABLE $tablename ($columns)";
 
-  // Выполняем SQL-запрос
     $db->exec($sql);
 
-  // Подготавливаем SQL-запрос для вставки данных в таблицу
   $sql = "INSERT INTO $tablename ($columns) VALUES ($values)";
   $stmt = $db->prepare($sql);
 
-  // Заполняем таблицу данными из JSON-файла
-  $count = 0; // Счетчик для хранения числа записей
+  $count = 0;
   foreach ($data as $row) {
-    // Присваиваем значения параметрам с именами полей
     foreach ($fields as $field) {
       $stmt->bindValue(":$field", $row[$field]);
     }
-    // Выполняем SQL-запрос
     $stmt->execute();
-    // Увеличиваем счетчик
     $count++;
   }
 
-  // Выводим сообщение об успешном импорте
   echo "Файл с данными получен из $url и обработан. Создана таблица $tablename и $count записей в ней.";
 } else {
-  // Выводим сообщение об ошибке, если ссылка невалидна
   echo "Неверная ссылка на файл. Пожалуйста, введите корректную ссылку.";
 }
 ?>
